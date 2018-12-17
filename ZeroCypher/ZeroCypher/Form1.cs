bcb5ce4602cb125aaa1d1b6a6e7d4554025fc31a
INF2.Quinta <INF2.Quinta>
2018-12-17 11:42:53 +0100

    I know what I am doing. Trust me..

--- ZeroCypher/ZeroCypher/Form1.cs
+++ ZeroCypher/ZeroCypher/Form1.cs
@@ -9,11 +9,15 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 using ZeroCypher.Models;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
 
 namespace ZeroCypher {
     public partial class frmMain : Form {
         private SerialPort Serial = new SerialPort();
+        private StringBuilder buffer = new StringBuilder();
         private List<Packet> OutputBuffer = new List<Packet>();
+        JsonSchema JsonPacketValidator = new JsonSchemaGenerator().Generate(typeof(RecivedPacket)); 
 
         public frmMain() {
             InitializeComponent();
@@ -22,12 +26,28 @@ namespace ZeroCypher {
         private void frmMain_Load(object sender, EventArgs e) {
             CheckForIllegalCrossThreadCalls = false;
             UpdatePortList();
+            UpdateComboBoxList();
             Serial.DataReceived += Serial_DataReceived;
 
         }
+        private void BlockEncodingAndDecoding()
+        {
+
+        }
+        private void UpdateComboBoxList()
+        {
+            var obj = new string[] { "cesare", "trasposizione" };
+            cobEncryptionType.DataSource = obj;
+            cobDecryptionType.DataSource = obj;
+        }
 
         private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e) {
-            throw new NotImplementedException();
+            buffer.Append(Serial.ReadExisting());
+            JObject data = JObject.Parse(buffer.ToString());
+            if (data.IsValid(JsonPacketValidator))
+            {
+                RecivedPacket temp = RecivedPacket.Dserialize(buffer.ToString());
+            }
         }
 
         private void UpdatePortList() {
@@ -40,12 +60,21 @@ namespace ZeroCypher {
         }
 
         private void btnConnect_Click(object sender, EventArgs e) {
-            if (!String.IsNullOrWhiteSpace(cobPortList.SelectedItem.ToString()) && !String.IsNullOrWhiteSpace(txtBaudeRate.Text)) {
-                Serial.PortName = cobPortList.SelectedItem.ToString();
-                Serial.BaudRate = Convert.ToInt32(txtBaudeRate.Text);
-                Connect();
-            } else
-                MessageBox.Show("Fill all the values before connecting", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(cobPortList.SelectedItem.ToString()) && !String.IsNullOrWhiteSpace(txtBaudeRate.Text))
+                {
+                    Serial.PortName = cobPortList.SelectedItem.ToString();
+                    Serial.BaudRate = Convert.ToInt32(txtBaudeRate.Text);
+                    Connect();
+                }
+                else
+                    MessageBox.Show("Fill all the values before connecting", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         private void Connect() {
             if (Serial.IsOpen)
@@ -54,30 +83,52 @@ namespace ZeroCypher {
         }
 
         private void btnEncrypt_Click(object sender, EventArgs e) {
-            if (Serial.IsOpen) {
-                if (!String.IsNullOrWhiteSpace(TxtEncMessage.Text) && !String.IsNullOrWhiteSpace(txtEncryptionKey.Text) && !string.IsNullOrWhiteSpace(cobEncryptionType.SelectedItem.ToString())) {
-                    Packet pak = new Packet(TxtEncMessage.Text, txtEncryptionKey.Text, true, cobEncryptionType.SelectedItem.ToString(), "request");
-                    pak.SetHashCode();
-                    OutputBuffer.Add(pak);
-                    Serial.Write(Packet.Serialize(pak,false));
-                    txtConsole.AppendText($"Data sent to port: '{Serial.PortName}', to be encrypted, the data sent is: \r\n{Packet.Serialize(pak,true)} ");
+            try
+            {
+                if (Serial.IsOpen)
+                {
+                    if (!String.IsNullOrWhiteSpace(TxtEncMessage.Text) && !String.IsNullOrWhiteSpace(txtEncryptionKey.Text) && !string.IsNullOrWhiteSpace(cobEncryptionType.SelectedItem.ToString()))
+                    {
+                        Packet pak = new Packet(TxtEncMessage.Text, txtEncryptionKey.Text, true, cobEncryptionType.SelectedItem.ToString(), "request");
+                        pak.SetHashCode();
+                        OutputBuffer.Add(pak);
+                        Serial.Write(Packet.Serialize(pak, false)+ "\n");
+                        txtConsole.AppendText($"Data sent to port: '{Serial.PortName}', to be encrypted, the data sent is: \r\n{Packet.Serialize(pak, true)} ");
+                    }
                 }
-            } else {
-                MessageBox.Show("The Client is not connected to a serial port", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    MessageBox.Show("The Client is not connected to a serial port", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Select a value in the Combobox", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void brnDecryption_Click(object sender, EventArgs e) {
-            if (Serial.IsOpen) {
-                if (!String.IsNullOrWhiteSpace(txtMessageDecryption.Text) && !String.IsNullOrWhiteSpace(txtDecryptionKey.Text) && !string.IsNullOrWhiteSpace(cobDecryptionType.SelectedItem.ToString())) {
-                    Packet pak = new Packet(txtMessageDecryption.Text, txtDecryptionKey.Text, false, cobDecryptionType.SelectedItem.ToString(), "request");
-                    pak.SetHashCode();                 
-                    OutputBuffer.Add(pak);
-                    Serial.Write(Packet.Serialize(pak,false));
-                    txtConsole.AppendText($"Data sent to port: '{Serial.PortName}', to be decrypted, the data sent is:\r\n {Packet.Serialize(pak, true)} ");
+            try
+            {
+                if (Serial.IsOpen)
+                {
+                    if (!String.IsNullOrWhiteSpace(txtMessageDecryption.Text) && !String.IsNullOrWhiteSpace(txtDecryptionKey.Text) && !string.IsNullOrWhiteSpace(cobDecryptionType.SelectedItem.ToString()))
+                    {
+                        Packet pak = new Packet(txtMessageDecryption.Text, txtDecryptionKey.Text, false, cobDecryptionType.SelectedItem.ToString(), "request");
+                        pak.SetHashCode();
+                        OutputBuffer.Add(pak);
+                        Serial.Write(Packet.Serialize(pak, false)+"\n");
+                        txtConsole.AppendText($"Data sent to port: '{Serial.PortName}', to be decrypted, the data sent is:\r\n {Packet.Serialize(pak, true)} ");
+                    }
                 }
-            } else {
-                MessageBox.Show("The Client is not connected to a serial port", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    MessageBox.Show("The Client is not connected to a serial port", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Select a value in the Combobox", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
---
+++ ZeroCypher/ZeroCypher/Models/RecivedPacket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ZeroCypher.Models
+{
+    public class RecivedPacket
+    {
+
+        public int id { get; set; }
+        public string status { get; set;}
+
+        public RecivedPacket(int id, string status)
+        {
+            this.id = id;
+            this.status = status;
+        }
+            
+        public RecivedPacket()
+        {
+        }
+        public static string Serialize(RecivedPacket pak)
+        {
+            return JsonConvert.SerializeObject(pak);
+        }
+        public static RecivedPacket Dserialize(string pak)
+        {
+            return JsonConvert.DeserializeObject<RecivedPacket>(pak);
+        }
+    }
+}
