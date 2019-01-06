e9551bdd72bcb780a894b769680f2a924fa57498
Singh Parkaran Preet <parkaranpreet@yahoo.com>
2019-01-06 01:02:43 +0100

    Future self, please forgive me and don't hit me with the baseball bat again!

---
+++ ZeroCypher/CommandParser/Parser/CommandSyntax/Syntax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandParser.Parser.Models;
+using Newtonsoft.Json;
+
+namespace CommandParser.Parser.CommandSyntax {
+    public class Syntax {
+        
+        public Syntax() {
+            //BuildCommands();
+        }
+    }
+}
